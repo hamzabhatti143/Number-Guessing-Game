df71ff13b8bf5c8af9f9733f328480195e2b63f3
hamzalege <104676544+hamzalege@users.noreply.github.com>
2024-04-25 00:47:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+let answer = await inquirer.prompt([
+    {
+        name: "number",
+        message: "Enter Your Number between 1-6: ",
+        type: "number"
+    }
+]);
+if (answer.number === randomNumber) {
+    console.log(`Your Guess is right!!`);
+}
+else {
+    console.log(`Your Guess is Wrong  I won ${randomNumber}`);
+}
